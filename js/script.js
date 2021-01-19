28e6bfbd684cd2fe60dcefe6e33836678a4536e2
Icey Isaak <me@iceyisaak.com>
2021-01-19 14:28:25 +0100

    Added pokemonList

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-alert('Hello World');
-
-// favouriteFood
-let favouriteFood = 'Steaks';
-document.write(favouriteFood);
+let pokemonList = [
+ {
+  name: 'Bulbasaur',
+  height: 7,
+  types: [
+   'grass',
+   'poison'
+  ]
+ },
+ {
+  name: 'Squirtle',
+  height: 5,
+  types: [
+   'water'
+  ]
+ },
+ {
+  name: 'Pikachu',
+  height: 4,
+  types: [
+   'electric'
+  ]
+ }
+];
