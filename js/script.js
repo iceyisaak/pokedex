9581114ce5e0b8f30991c11c37f0aee72d2d6e94
Icey Isaak <me@iceyisaak.com>
2021-01-19 19:23:10 +0100

    Added logic to check on pokemon height and display list

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
+// Task 1.2
+// List out all pokemon
 let pokemonList = [
  {
   name: 'Bulbasaur',
-  height: 7,
+  height: 0.7,
   types: [
    'grass',
    'poison'
@@ -9,16 +11,52 @@ let pokemonList = [
  },
  {
   name: 'Squirtle',
-  height: 5,
+  height: 0.5,
   types: [
    'water'
   ]
  },
  {
   name: 'Pikachu',
-  height: 4,
+  height: 0.4,
   types: [
    'electric'
   ]
+ },
+ {
+  name: 'Kangaskhan',
+  height: 2.2,
+  types: [
+   'normal'
+  ]
  }
 ];
+
+// Task 1.3
+// Create a List
+document.write('<ul>');
+
+// Looping through the pokemonList
+for (i = 0; i < pokemonList.length; i++) {
+
+ // pokemon with height > 1?
+ pokemonList[i].height > 1 ?
+
+  // Display the list with label next to the list
+  document.write(`
+  <li class="list mb-3">
+  ${i + 1}. ${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that's big!
+  </li> 
+  `)
+  :
+  // Else, just display the list
+  document.write(`
+ <li class="list mb-3">
+ ${i + 1}.  ${pokemonList[i].name} (height: ${pokemonList[i].height})
+ </li> 
+ `);
+}
+
+// End of the List
+document.write('</ul>');
+
