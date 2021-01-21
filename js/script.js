a010f448ce579957eaa5e5328ac9c4fb8246e9a6
Icey Isaak <me@iceyisaak.com>
2021-01-21 18:49:20 +0100

    Added Event Listener + Clean up code

--- js/script.js
+++ js/script.js
@@ -2,90 +2,114 @@
 // 
 let pokemonRepository = (() => {
 
- // List out all pokemon
- let pokemonList = [
-  {
-   name: 'Bulbasaur',
-   height: 0.7,
-   types: [
-    'grass',
-    'poison'
-   ]
-  },
-  {
-   name: 'Squirtle',
-   height: 0.5,
-   types: [
-    'water'
-   ]
-  },
-  {
-   name: 'Pikachu',
-   height: 0.4,
-   types: [
-    'electric'
-   ]
-  },
-  {
-   name: 'Kangaskhan',
-   height: 2.2,
-   types: [
-    'normal'
-   ]
+  // List out all pokemon
+  let pokemonList = [
+    {
+      name: 'Bulbasaur',
+      height: 0.7,
+      types: [
+        'grass',
+        'poison'
+      ]
+    },
+    {
+      name: 'Squirtle',
+      height: 0.5,
+      types: [
+        'water'
+      ]
+    },
+    {
+      name: 'Pikachu',
+      height: 0.4,
+      types: [
+        'electric'
+      ]
+    },
+    {
+      name: 'Kangaskhan',
+      height: 2.2,
+      types: [
+        'normal'
+      ]
+    }
+  ];
+
+  // Why is this not working?
+  function addv(item) {
+    // If item is NOT object
+    if (typeof item !== Object) {
+      // Stop program execution
+      return;
+    }
   }
- ];
 
- // Why is this not working?
- function addv(item) {
-  if (typeof item !== Object) {
-   // Stop program execution
-   return;
+  // Get all pokemons in the list
+  function getAll() {
+    return pokemonList;
   }
- }
 
- // Get all pokemons in the list
- function getAll() {
-  return pokemonList;
- }
+  // Add new a pokemon
+  function add(item) {
 
- // Add new a pokemon
- function add(item) {
+    // Validate data type (NOT WORKING)
+    addv(item);
+    // return the list with a newly added pokemon
+    return pokemonList.push(item);
+  }
 
-  // Validate data type
-  addv(item);
-  // return the list with a newly added pokemon
-  return pokemonList.push(item);
- }
+  // Add item to list
+  function addListItem(pokemon, index) {
+
+    let list = document.querySelector('ul');
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+
+    // Display text, if pokemon with height > 1
+    button.innerText = pokemon.height > 1.0 ?
+
+      //   // Display the list with label 
+      `${index + 1}. ${pokemon.name} (${pokemon.height}m) - Wow that's big!`
+      :
+      //   // Display the list with NO label 
+      `${index + 1}. ${pokemon.name} (${pokemon.height}m)`;
 
 
+    // Apply the styling
+    button.classList.add('button', 'list', 'mb-3');
 
- return {
-  getAll: getAll,
-  add: add
- };
+
+    // Append the list
+    listItem.appendChild(button);
+    list.appendChild(listItem);
+
+    // Listen for 'Click' in order to trigger showDetails()
+    button.addEventListener('click', () => showDetails(pokemon));
+
+  }
+
+  // showDetails()
+  function showDetails(pokemon) {
+
+    //log the pokemon clicked
+    console.log(pokemon);
+  }
+
+  return {
+    getAll: getAll,
+    add: add,
+    addListItem: addListItem
+  };
 
 })();
-
 
 
 // pokemonList.forEach() 
 pokemonRepository.getAll().forEach((pokemon, index) => {
 
- // pokemon with height > 1?
- pokemon.height > 1 ?
-  // Display the list with label 
-  document.write(`
-   <li class="list mb-3">
-    ${index + 1}. ${pokemon.name} (height: ${pokemon.height}) - Wow, that's big!
-   </li> 
-`)
-  :
-  // Else, display just the list
-  document.write(`
-  <li class="list mb-3">
-   ${index + 1}.  ${pokemon.name} (height: ${pokemon.height})
-  </li> 
-  `);
+  // Add item to list
+  pokemonRepository.addListItem(pokemon, index);
+
 });
 
 
